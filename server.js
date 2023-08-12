8984898c84c37d4cdf230c0a81499f2be1a8e58d
mistank <milan.stankovic550@gmail.com>
2023-08-12 14:58:22 +0200

    promena

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const questions = [
         answers: ['Berlin', 'Pariz', 'Madrid', 'Lisabon']
     },
     {
-        question: 'Koji vitamin se najviše nalazi u narandži AAAAA?',
+        question: 'Koji vitamin se najviše nalazi u narandži AAAAAbbbbbbb?',
         correctAnswer: 4,
         answers: ['Vitamin A', 'Vitamin D', 'Vitamin B', 'Vitamin C']
     },
